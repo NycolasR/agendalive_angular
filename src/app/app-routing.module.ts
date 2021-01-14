0823501c810135043c1f4ddff6a97d86af376690
NycolasR <nycolasramon3@gmail.com>
2021-01-14 01:29:31 -0300

    Preparo do ambiente + primeiro componente criado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// Componentes importados
+import { HomeComponent } from './views/home/home.component';
+
+const routes: Routes = [
+  { // Cada roteamento possui um path e um component
+    path: "", // Caminho raiz -> default
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
