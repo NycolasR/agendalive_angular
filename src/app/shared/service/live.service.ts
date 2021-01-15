c1aa547ce5438e9319a1c459594a3d5889f270df
NycolasR <nycolasramon3@gmail.com>
2021-01-15 18:54:55 -0300

    Criando uma service

---
+++ src/app/shared/service/live.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { ResponsePageable } from '../model/responsePageable.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LiveService {
+
+  apiUrl = "http://localhost:8080/lives";
+
+  // Utilizado para requisições POST
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  // Observable (retorno): funcionalidade da biblioteca rgjs (já instalada ao criar
+  // uma aplicação Angular). Próprio para trabalhar com transição de dados assíncrona
+  // (por trabalhar com requisição de dados a uma API externa, faz-se necessário o uso de
+  // requisições assíncronas). Quando os dados estiverem prontos, haverá um observable
+  // para receber esses dados e será executado o que foi definido no subscriable.
+
+  public getLivesWithFlag(flag: string): Observable<ResponsePageable> {
+    return this.httpClient.get<ResponsePageable>(this.apiUrl + '?flag=' + flag);
+  }
+}
