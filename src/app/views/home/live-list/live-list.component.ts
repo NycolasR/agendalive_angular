b1ad6d65b873294ce523165804b8d8574091bacd
NycolasR <nycolasramon3@gmail.com>
2021-01-16 19:51:16 -0300

    Obtendo dados da API externa (back-end)

--- src/app/views/home/live-list/live-list.component.ts
+++ src/app/views/home/live-list/live-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Live } from 'src/app/shared/model/live.model';
+import { LiveService } from 'src/app/shared/service/live.service';
 
 @Component({
   selector: 'app-live-list',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LiveListComponent implements OnInit {
 
-  constructor() { }
+  // Lives que já aconteceram
+  livesPrevious: Live[];
+  // Lives que já ainda não aconteceram
+  livesNext: Live[];
+
+  constructor(
+    public liveService: LiveService
+  ) {
+    this.livesPrevious = [];
+    this.livesNext = [];
+  }
 
   ngOnInit(): void {
+    // Para que sempre que este componente for inicializado,
+    // ele vai chamar este método:
+    this.getLives();
+  }
+
+  getLives() {
+    // this.liveService.getLivesWithFlag('previous') retornará um observable;
+    // No subscribe() é definido o que acontece com os dados provenientes da requisição
+    this.liveService.getLivesWithFlag('previous').subscribe(data => {
+      this.livesPrevious = data.content;
+      // Será retornado uma paginação com as lives, daí o content.
+      console.log(this.livesPrevious);
+    });
+
+    this.liveService.getLivesWithFlag('next').subscribe(data => {
+      this.livesNext = data.content;
+      // Será retornado uma paginação com as lives, daí o content.
+      console.log(this.livesNext);
+    });
   }
 
 }
